16ccae81030cab0c6802d0fe94c2851589be333c
Andrew McKinney <andrewryanmckinney@gmail.com>
2020-01-19 20:53:53 -0500

    finished standard SQL work

--- EmployeeSQL/scripts/Analytics.sql
+++ EmployeeSQL/scripts/Analytics.sql
@@ -1,5 +1,66 @@
+-- Author: Andrew McKinney
+-- DATE: 01/19/2020
+-- Employees SQL Select statements
+
+
+
+-- 1. List the following details of each employee: employee number, last name, first name, gender, and salary.
 SELECT e.emp_no, e.last_name, e.first_name, e.gender, s.salary 
 FROM employees AS e
-LEFT JOIN salaries AS s ON e.emp_no = s.emp_no
+LEFT JOIN salaries AS s ON e.emp_no = s.emp_no;
 
-select * from dept_emp
+
+-- 2. List employees who were hired in 1986.
+SELECT *
+FROM employees
+WHERE hire_date < '1987-01-01' AND hire_date >= '1986-01-01'
+ORDER BY hire_date ASC
+
+
+-- 3. List the manager of each department with the following information: department number, department name, the manager's employee number, last name, first name, and start and end employment dates.
+SELECT m.dept_no, d.dept_name, m.emp_no, e.first_name, e.last_name, m.from_date, m.to_date
+FROM dept_manager AS m
+LEFT JOIN departments AS d ON m.dept_no = d.dept_no
+LEFT JOIN employees AS e ON m.emp_no = e.emp_no
+
+
+-- 4. List the department of each employee with the following information: employee number, last name, first name, and department name.
+SELECT e.emp_no, e.last_name, e.first_name, d.dept_name
+FROM employees AS e
+LEFT JOIN dept_emp AS de ON e.emp_no = de.emp_no
+LEFT JOIN departments AS d ON de.dept_no = d.dept_no
+
+
+-- 5. List all employees whose first name is "Hercules" and last names begin with "B."
+SELECT *
+FROM employees
+WHERE first_name = 'Hercules' AND last_name LIKE 'B%'
+
+
+-- 6. List all employees in the Sales department, including their employee number, last name, first name, and department name.
+SELECT e.emp_no, e.last_name, e.first_name, d.dept_name
+FROM employees AS e
+LEFT JOIN dept_emp AS de ON e.emp_no = de.emp_no
+LEFT JOIN departments AS d ON de.dept_no = d.dept_no
+WHERE d.dept_name = 'Sales'
+
+
+-- 7. List all employees in the Sales and Development departments, including their employee number, last name, first name, and department name.
+SELECT e.emp_no, e.last_name, e.first_name, d.dept_name
+FROM employees AS e
+LEFT JOIN dept_emp AS de ON e.emp_no = de.emp_no
+LEFT JOIN departments AS d ON de.dept_no = d.dept_no
+WHERE d.dept_name IN ('Sales', 'Development')
+
+
+-- 8. In descending order, list the frequency count of employee last names, i.e., how many employees share each last name.
+SELECT last_name, count(last_name) AS emp_count
+FROM employees
+GROUP BY last_name
+
+
+-- Getting Fooled:
+SELECT * 
+FROM employees 
+WHERE emp_no = 499942
+
--- EmployeeSQL/scripts/schema.sql
+++ EmployeeSQL/scripts/schema.sql
@@ -9,21 +9,21 @@ DROP TABLE IF EXISTS employees;
 DROP TABLE IF EXISTS salaries;
 DROP TABLE IF EXISTS titles;
 
-
+-- NOTE: I was instructed to comment out Keys by TA due to import conflicts of CSV data
 
 CREATE TABLE departments (
   dept_no VARCHAR(4) NOT NULL,
-  dept_name VARCHAR(50),
-  PRIMARY KEY (dept_no)
+  dept_name VARCHAR(50)
+  -- PRIMARY KEY (dept_no)
 );
 
 CREATE TABLE dept_emp (
   emp_no INTEGER NOT NULL,
   dept_no VARCHAR(4) NOT NULL,
   from_date DATE,
-  to_date DATE,
-  PRIMARY KEY (emp_no),
-  FOREIGN KEY (dept_no) REFERENCES departments(dept_no)
+  to_date DATE
+  -- PRIMARY KEY (emp_no),
+  -- FOREIGN KEY (dept_no) REFERENCES departments(dept_no)
 );
 
 CREATE TABLE employees (
@@ -32,36 +32,36 @@ CREATE TABLE employees (
     first_name VARCHAR(255),
     last_name VARCHAR(255),
     gender VARCHAR(1),
-    hire_date DATE,
-    PRIMARY KEY (emp_no),
-    FOREIGN KEY (emp_no) REFERENCES dept_emp(emp_no)
+    hire_date DATE
+    -- PRIMARY KEY (emp_no),
+    -- FOREIGN KEY (emp_no) REFERENCES dept_emp(emp_no)
 );
 
 CREATE TABLE dept_manager (
   dept_no VARCHAR(4) NOT NULL,
   emp_no INTEGER NOT NULL,
   from_date DATE,
-  to_date DATE,
-  PRIMARY KEY (dept_no),
-  FOREIGN KEY (dept_no) REFERENCES departments(dept_no),
-  FOREIGN KEY (emp_no) REFERENCES employees(emp_no)
+  to_date DATE
+  -- PRIMARY KEY (dept_no),
+  -- FOREIGN KEY (dept_no) REFERENCES departments(dept_no),
+  -- FOREIGN KEY (emp_no) REFERENCES employees(emp_no)
 );
 
 CREATE TABLE salaries (
   emp_no INTEGER NOT NULL,
   salary INTEGER,
   from_date DATE,
-  to_date DATE,
-  PRIMARY KEY (emp_no),
-  FOREIGN KEY (emp_no) REFERENCES employees(emp_no)
+  to_date DATE
+  -- PRIMARY KEY (emp_no),
+  -- FOREIGN KEY (emp_no) REFERENCES employees(emp_no)
 );
 
 CREATE TABLE titles (
   emp_no INTEGER NOT NULL,
   title VARCHAR(255),
   from_date DATE,
-  to_date DATE,
-  PRIMARY KEY (emp_no),
-  FOREIGN KEY (emp_no) REFERENCES employees(emp_no)
+  to_date DATE
+  -- PRIMARY KEY (emp_no),
+  -- FOREIGN KEY (emp_no) REFERENCES employees(emp_no)
 );
 
